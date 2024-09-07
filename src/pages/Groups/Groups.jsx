0961a7575a75f71b565644885e183ae54abe2755
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-07 19:59:39 +0300

    Update orders.json

--- src/pages/Groups/Groups.jsx
+++ src/pages/Groups/Groups.jsx
@@ -3,5 +3,5 @@ import { useLocation } from "react-router-dom";
 export default function Groups() {
   let { state } = useLocation();
 
-  return <div>Groups Component {state.length}</div>;
+  return <div>Groups {state[0].type}</div>;
 }
