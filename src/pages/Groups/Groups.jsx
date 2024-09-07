3a33032fd99b8fcea4b9e992d847b4a9d09af0ad
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-07 20:25:27 +0300

    Update Users Page

--- src/pages/Groups/Groups.jsx
+++ src/pages/Groups/Groups.jsx
@@ -3,5 +3,5 @@ import { useLocation } from "react-router-dom";
 export default function Groups() {
   let { state } = useLocation();
 
-  return <div>Groups {state[0].type}</div>;
+  return <div>Groups {state ? state[0]?.type : null}</div>;
 }
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,3 +1,11 @@
+import usersImage from "../../assets/users.png";
+import styles from "./Users.module.css";
+
 export default function Users() {
-  return <div>Users Component</div>;
+  return (
+    <div className={styles.userPageWrapper}>
+      <h3>Users Page</h3>
+      <img src={usersImage} className={styles.usersImage} alt="usrers image" />
+    </div>
+  );
 }
