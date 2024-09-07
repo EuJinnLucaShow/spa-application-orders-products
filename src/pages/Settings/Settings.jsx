b5cff9698a5dc0c8d830aa5231d4011d97fc81cd
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-07 20:40:42 +0300

    Update Setting Page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,3 +1,15 @@
+import settingsImg from "../../assets/settings.png";
+import styles from "./Settings.module.css";
+
 export default function Settings() {
-  return <div>Settings Component</div>;
+  return (
+    <div className={styles.settingPageWrapper}>
+      <h3>Settings Page</h3>
+      <img
+        src={settingsImg}
+        className={styles.settingImage}
+        alt="setting image"
+      />
+    </div>
+  );
 }
