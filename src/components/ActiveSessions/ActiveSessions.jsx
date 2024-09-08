acb22b25bb8cef5c1f204472e68376e35a37491d
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-08 15:38:30 +0300

    Update ActiveSessions.jsx
    Added transports: ["websocket", "polling"],

--- src/components/ActiveSessions/ActiveSessions.jsx
+++ src/components/ActiveSessions/ActiveSessions.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+import styles from "./ActiveSessions.module.css";
 
 export default function ActiveSessions() {
   const [activeSessions, setActiveSessions] = useState(0);
 
   useEffect(() => {
     const socket = io("https://orders-products-backend.onrender.com", {
+      transports: ["websocket", "polling"],
       withCredentials: true,
     });
 
@@ -19,7 +21,7 @@ export default function ActiveSessions() {
   }, []);
 
   return (
-    <h5>
+    <h5 className={styles.activeSession}>
       Active Sessions: <span>{activeSessions}</span>
     </h5>
   );
