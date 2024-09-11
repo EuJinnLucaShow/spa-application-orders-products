c4b1073ae53aa7635a08f4134e15591b1230d6fa
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-11 10:42:41 +0300

    Update Incoming.jsx

--- src/pages/Incoming/Incoming.jsx
+++ src/pages/Incoming/Incoming.jsx
@@ -8,13 +8,15 @@ import Orders from "../../components/Orders/Orders";
 import iconBtn from "../../assets/plus.svg";
 import styles from "./Incoming.module.css";
 import ListProducts from "../../components/ListProducts/ListProducts";
+import PopUp from "../../components/PopUp/PopUp";
 
 export default function Incoming() {
   const dispatch = useDispatch();
   const orders = useSelector(selectOrders);
   const [openProducts, setOpenProducts] = useState(true);
   const [date, setDate] = useState([]);
-  const [activeItem, setActiveItem] = useState('');
+  const [activeItem, setActiveItem] = useState("");
+  const [ModalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
     toast.promise(dispatch(fetchOrders()), {
@@ -28,35 +30,38 @@ export default function Incoming() {
   }, [dispatch, orders.length]);
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.titleBox}>
-        <button className={styles.addBtn}>
-          <img src={iconBtn} className={styles.iconBtn} alt="add button" />
-        </button>
-        <h3 className={styles.title}>
-          Incoming / <span>{orders.length}</span>
-        </h3>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.titleBox}>
+          <button className={styles.addBtn} onClick={() => setModalOpen(true)}>
+            <img src={iconBtn} className={styles.iconBtn} alt="add button" />
+          </button>
+          <h3 className={styles.title}>
+            Incoming / <span>{orders.length}</span>
+          </h3>
+        </div>
+        {openProducts ? (
+          <ul className={styles.ordersList}>
+            {orders.map((order, index) => (
+              <Orders
+                key={index}
+                order={order}
+                openProduct={() => setOpenProducts(false)}
+                optionProducts={(products) => setDate(products)}
+                isActive={(id) => setActiveItem(id)}
+              />
+            ))}
+          </ul>
+        ) : (
+          <ListProducts
+            orders={orders}
+            openProduct={() => setOpenProducts(true)}
+            date={date}
+            activeItem={activeItem}
+          />
+        )}
       </div>
-      {openProducts ? (
-        <ul className={styles.ordersList}>
-          {orders.map((order, index) => (
-            <Orders
-              key={index}
-              order={order}
-              openProduct={() => setOpenProducts(false)}
-              optionProducts={(products) => setDate(products)}
-              isActive={(id) => setActiveItem(id)}
-            />
-          ))}
-        </ul>
-      ) : (
-        <ListProducts
-          orders={orders}
-          openProduct={() => setOpenProducts(true)}
-          date={date}
-          activeItem={activeItem}
-        />
-      )}
-    </div>
+      <PopUp isOpen={ModalOpen} onClose={() => setModalOpen(false)} />
+    </>
   );
 }
