a3ae8c80019843c4660dd152b290d458d207717f
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-11 19:07:22 +0300

    Groups Page placeholder
    Groups Page placeholder

--- src/pages/Groups/Groups.jsx
+++ src/pages/Groups/Groups.jsx
@@ -1,3 +1,11 @@
+import groupImg from "../../assets/ProductGroup.jpeg";
+import styles from "./Groups.module.css";
+
 export default function Groups() {
-  return <div>Groups</div>;
+  return (
+    <div className={styles.groupsPageWrapper}>
+      <h3>Groups Page</h3>
+      <img src={groupImg} className={styles.groupsImage} alt="groups image" />
+    </div>
+  );
 }
