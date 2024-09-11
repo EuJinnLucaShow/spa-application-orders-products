4ead0b0c2f26f3340274fda51f84ed5a5d60c393
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2024-09-11 11:57:11 +0300

    The Products component displays all products.
    The Products component displays all products.

---
+++ src/components/ProductsItem/ProductsItem.jsx
@@ -0,0 +1,44 @@
+import PropTypes from "prop-types";
+
+import trashIcon from "../../assets/trash.svg";
+import styles from "./ProductsItem.module.css";
+import PopUp from "../PopUp/PopUp";
+import { useState } from "react";
+
+export default function ProductsItem({ item }) {
+  const [ModalOpen, setModalOpen] = useState(false);
+
+  return (
+    <>
+      <li className={styles.productItem}>
+        <div className={styles.pointProduct}></div>
+        <img
+          src={item.photo}
+          className={styles.imgProduct}
+          alt="product image"
+        />
+        <div className={styles.titleProduct}>
+          <h4>{item.type}</h4>
+          <p>{item.serialNumber}</p>
+        </div>
+        <p className={styles.accessible}>Accessible</p>
+        <button className={styles.trashBtn} onClick={() => setModalOpen(true)}>
+          <img
+            src={trashIcon}
+            className={styles.trashIcon}
+            alt="trash button"
+          />
+        </button>
+      </li>
+      <PopUp isOpen={ModalOpen} onClose={() => setModalOpen(false)} />
+    </>
+  );
+}
+
+ProductsItem.propTypes = {
+  item: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    photo: PropTypes.string.isRequired,
+    serialNumber: PropTypes.number.isRequired,
+  }).isRequired,
+};
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,3 +1,33 @@
-export default function Products() { 
-    return <div>Products Component</div>;
+import { useSelector } from "react-redux";
+import { useState } from "react";
+
+import ProductsItem from "../../components/ProductsItem/ProductsItem";
+import { selectOrders } from "../../redux/selectors";
+import iconBtn from "../../assets/plus.svg";
+import styles from "./Products.module.css";
+import PopUp from "../../components/PopUp/PopUp";
+
+export default function Products() {
+  const [ModalOpen, setModalOpen] = useState(false);
+  const orders = useSelector(selectOrders);
+  const allProducts = orders.flatMap((order) => order.products);
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.titleBox}>
+        <button className={styles.addBtn} onClick={() => setModalOpen(true)}>
+          <img src={iconBtn} className={styles.iconBtn} alt="add button" />
+        </button>
+        <h3 className={styles.title}>
+          Products / <span>{allProducts.length}</span>
+        </h3>
+      </div>
+      <ul className={styles.productsList}>
+        {allProducts.map((product) => (
+          <ProductsItem key={product.id} item={product} />
+        ))}
+      </ul>
+      <PopUp isOpen={ModalOpen} onClose={() => setModalOpen(false)} />
+    </div>
+  );
 }
